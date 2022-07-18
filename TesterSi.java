eac9652825932a17f60070b46db7ac7f0fe3b82c
Vishnu Senthilkumar <vishnu.s@prodapt.com>
2022-07-18 17:50:24 +0530

    Tester

---
+++ TesterSi.java
@@ -0,0 +1,12 @@
+package com.prodapt.OOPbasics;
+
+public class TesterSi {
+	public static void main(String[] args) {
+		SimpleIntrest.principle = 2000;
+		SimpleIntrest.rate = 10;
+		SimpleIntrest.time = 1;
+		SimpleIntrest.simpleInterest();
+	}
+
+}
+
